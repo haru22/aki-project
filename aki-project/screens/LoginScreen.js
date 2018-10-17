4728616c58e759bb418c0a43b23ff53b302b87ef
haru22 <y.a.m.aa0867@gmail.com>
2018-10-16 17:19:55 -0700

    login page

--- aki-project/screens/LoginScreen.js
+++ aki-project/screens/LoginScreen.js
@@ -1,24 +1,79 @@
 import React from 'react';
-import { ScrollView, View, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+
 
 export default class LoginScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Sign Up</Text>
-        
+     
+        <View style={styles.logoContainer}> 
+        	<Image 
+        		style={styles.logo}
+        		source={require('../assets/images/logo.png')}
+	        	/>
+	        <View style={styles.inputContainer}>
+	        <TextInput 
+	        	placeholder="username or email      "
+	        	style={styles.input}
+	        	/>
+	         <TextInput 
+	         	placeholder="password       "
+	         	style={styles.input}
+	        	/>
+	        <TouchableOpacity style={styles.buttonContainer}>
+	        	<Text style={styles.buttonText}>JOIN</Text>
+	        </TouchableOpacity>
+	         <TouchableOpacity style={styles.buttonContainer}>
+	        	<Text style={styles.buttonText}>CREATE</Text>
+	        </TouchableOpacity>
+	        </View>
+   		</View>
+
       </View>
+    	
     );
   }
 }
 
 const styles = StyleSheet.create({
 	container: {
-		//alignItems: 'center',
+		alignItems: 'center',
 		justifyContent: 'center',
 		flex: 1,
+		backgroundColor: '#0071FF',
 	},
 	text: {
-		color: 'red',
-	}
+		
+	},
+	logoContainer: {
+		alignItems: 'center',
+		flexGrow: 1,
+		justifyContent: 'center',
+	},
+	logo: {
+		width: 100,
+		height: 100,
+	},
+	input: {
+		height: 40,
+		backgroundColor: '#90D6FF',
+		marginBottom: 20,
+		color: '#FFF',
+		paddingHorizontal: 10
+	},
+	inputContainer: {
+		padding: 20,
+	},
+	buttonContainer: {
+		backgroundColor: '#2980b9',
+		paddingVertical: 15,
+		paddingHorizontal: 10
+	},
+	buttonText: {
+		textAlign: 'center',
+		color: '#FFFFFF',
+		paddingHorizontal: 10,
+	},
 })
